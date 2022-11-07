fc008b1ebef350b54b142ea3101c6824b368e599
Joo-hyun Kang <59409922+JOOHYUN-KANG@users.noreply.github.com>
2022-11-07 15:02:21 +0900

    Add focal loss parameters (2, 0.25)

--- Siam-NestedUNet/utils/losses.py
+++ Siam-NestedUNet/utils/losses.py
@@ -8,8 +8,9 @@ def hybrid_loss(predictions, target):
     """Calculating the loss"""
     loss = 0
 
-    # gamma=0, alpha=None --> CE
-    focal = FocalLoss(gamma=0, alpha=None)
+    
+    focal = FocalLoss(gamma=0, alpha=None) # gamma=0, alpha=None --> CE
+    # focal = FocalLoss(gamma=2, alpha=0.25)
 
     for prediction in predictions:
 
